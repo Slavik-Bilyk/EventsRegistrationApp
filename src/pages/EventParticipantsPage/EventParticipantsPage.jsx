a4afc4b1207490e84b05cc9cf450cc7cfd4f39a3
Slavik-Bilyk <Slavataschor@ukr.net>
2024-09-26 21:42:20 +0300

    add participantsPage

--- src/pages/EventParticipantsPage/EventParticipantsPage.jsx
+++ src/pages/EventParticipantsPage/EventParticipantsPage.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import styles from './EventParticipantsPage.module.css'
 
 const EventParticipantsPage = () => {
   return (
     <div>
-      EventParticipantsPage
+     <h1>"Art Exhibition" participants</h1>
+     <div className={styles.blockFilter}>
+        <h2>Filters: </h2>
+        <form className={styles.form}>
+            <label className={styles.label}>
+                Full name
+                <input className={styles.input}/>
+            </label>
+            
+            <label className={styles.label}>
+                email
+                <input className={styles.input}/>
+            </label>
+
+            <button type='submit' className={styles.button}>Reset fillters</button>
+        </form>
+     </div>
     </div>
   )
 }
